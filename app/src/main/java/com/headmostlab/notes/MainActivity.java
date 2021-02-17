d94b8734e22938236360115c69295619bc9db6e2
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-02-18 00:37:30 +0500

    NavigationDrawer and ToolBar have been embedded in the app.

--- app/src/main/java/com/headmostlab/notes/MainActivity.java
+++ app/src/main/java/com/headmostlab/notes/MainActivity.java
@@ -3,17 +3,26 @@ package com.headmostlab.notes;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import com.headmostlab.notes.databinding.ActivityMainBinding;
 import com.headmostlab.notes.ui.notelist.NoteListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        initView();
 
         if (savedInstanceState != null) {
             return;
@@ -23,6 +32,40 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.container, NoteListFragment.newNoteListFragment())
                 .commitNow();
+    }
+
+    private void initView() {
+        initToolBar();
+        initDrawer();
+    }
+
+    private void initToolBar() {
+        setSupportActionBar(binding.appBarMain.toolbar);
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                binding.drawer, binding.appBarMain.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        binding.navView.setNavigationItemSelectedListener(item -> {
+            if (navigateFragment(item.getItemId())) {
+                binding.drawer.closeDrawer(GravityCompat.START);
+            }
+            return false;
+        });
+
+    }
+
+    private boolean navigateFragment(int menuId) {
+        switch (menuId) {
+            case R.id.action_about:
+                // TODO: 2/17/2021
+                return true;
+        }
+        return false;
     }
 
 }
